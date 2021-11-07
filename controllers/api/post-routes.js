e8579ba8c26c574a1b7cb8ab63ab0ac0d6064f04
Catherine <catheneghan@gmail.com>
2021-11-07 15:42:25 -0800

    created post routes to get all posts and by id, or to create a new post, also get all posts by user id

---
+++ controllers/api/post-routes.js
@@ -0,0 +1,92 @@
+const router = require('express').Router();
+const {User, Post, Reply} = require('../../models');
+
+// Get all posts
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: ['id', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// get a single post
+router.get('/:id', (req,res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({message: 'No post with that id'});
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// get all posts for a single user
+router.get('/user/:id', (req, res) => {
+    Post.findAll({
+        where: {
+            user_id: req.params.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if (!dbPostData) {
+                res.status(404).json({message: 'No posts found by that user'});
+                return;
+            }
+            res.json(dbPostData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+})
+
+// Create a post
+router.post('/', (req, res) => {
+    //
+    Post.create({
+        title: req.body.title,
+        body: req.body.body,
+        user_id: req.body.user_id
+    })
+        .then(dbPostData => res.json(dbPostData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+
+module.exports = router;
