52f9574a491cf44f72551bb2a226341268151fab
Catherine <catheneghan@gmail.com>
2021-11-11 21:17:15 -0800

    handlebars renders the homepage with all posts

--- controllers/html/index.js
+++ controllers/html/index.js
@@ -1,7 +1,64 @@
 const router = require('express').Router();
+const sequelize = require('../../config/connection');
+const { Post, User, Reply} = require('../../models');
 
+// Homepage 
 router.get('/', (req, res) => {
-    res.render('homepage');
+    Post.findAll({
+        attributes: ['id', 'title', 'created_at'],
+        order: [['created_at', 'DESC']],
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+        .then(dbAllPost => {
+            
+            const posts = dbAllPost.map(post => post.get({ plain: true }));
+            
+            res.render('homepage', { posts });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        })
+});
+
+// single post page
+router.get('/single-post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: User,
+                attributes: ['username']
+            },
+            {
+                model: Reply,
+                attributes: ['body', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            }
+        ]
+    })
+        .then(dbPostData => {
+            if(!dbPostData) {
+                res.status(404).json({message: 'No post found with this id'});
+                return document.location('/');
+            }
+            const post = dbPostData.map(post => post.get({plain: true}));
+            res.render('single-post', { post });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
 module.exports = router;
